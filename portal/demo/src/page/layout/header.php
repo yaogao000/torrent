2b2f1d2eeaabaa765bf4843d38129f35e0b13545
yaogao000 <yaogao000@163.com>
2015-11-24 10:07:49 +0800

    update header.php

---
+++ portal/demo/src/page/layout/header.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8"/>
+<meta http-equiv="Cache-Control" content="no-transform "/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta content="telephone=no",email="no" name="format-detection" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<!-- Bootstrap -->
+<link href="<?php echo ASSETS_PATH; ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
